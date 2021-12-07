651115fffa530f4b42f6a9ca5705092c69729dc4
Anil Kumar <786.anil.potlapally@gmail.com>
2021-12-07 06:31:48 +0530

    list displaying of dir and its filenames

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  let [files, setFiles] = useState();
+
   useEffect(() => {
     fetch("http://localhost:5000/api/videoslist", {
       // method: "GET",
@@ -13,14 +15,34 @@ function App() {
         return response.json();
       })
       .then(function (data) {
-        alert(data);
+        setFiles(data);
+        //alert(JSON.stringify(data));
       })
       .catch(function (error) {});
   }, []);
 
   return (
     <div className="App">
-      <div className="video-playlist"></div>
+      <div className="video-playlist">
+        <div>
+          {files?.map((d) => {
+            //console.log(JSON.stringify(f.fileName));
+            return (
+              <div class="">
+                <h3> {d.directoryName}</h3>
+                <ul className="directories">
+                  {d.fileNames.map((f) => {
+                    return <li> {f} </li>;
+                  })}
+                </ul>
+              </div>
+            );
+          })}
+        </div>
+        <div className="video">
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 }
